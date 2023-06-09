c2973f52d1a91413ebcc9cb52d2b2a6e07c65e63
SandhyaRajendran <sandhyadckap@gmail.com>
2023-06-09 16:58:59 +0530

    partlally completed

---
+++ connection.php
@@ -0,0 +1,32 @@
+<?php
+
+class Database
+{
+    public $db;
+
+    public function __construct()
+    {
+        try{
+            $this->db = new PDO
+            (
+                'mysql:host=127.0.0.1;dbname=image_upload',
+                'admin',
+                'welcome'
+            );
+        }
+        
+        catch(PDOException $e){
+            // die("connection error");
+           echo $e->getMessage();
+        }
+    }
+
+    public function query($query)
+    {
+        $statement = $this->db->prepare($query);
+        $statement->execute($statement);
+
+        return $statement;
+    }
+}
+
---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+require 'index.html';
+require 'connection.php';
+
+try{
+    $app['db'] = (new Database())->db;
+
+
+    // print_r($allDetails);
+}
+catch(PDOException $e){
+    die("connection problem");
+}
